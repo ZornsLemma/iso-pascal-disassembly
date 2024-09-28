398a7be812ce09c6b6e5ddc02ec1aa3f6da251d9
Steven Flintham <sgf@lemma.co.uk>
2024-09-28 14:01:37 +0100

    WIP

--- go.sh
+++ go.sh
@@ -11,3 +11,8 @@ beebasm -o build/pascal-2.rom -i source/pascal-2.asm -v > build/pascal-2.lst
 cmp orig/pascal-2.rom build/pascal-2.rom || (echo pascal-2.rom not rebuilt correctly; exit 1)
 
 python3 compiler.py > source/compiler.asm
+
+# Generate versions of the ROMs with what I believe to be free space cleared, to
+# help confirm these regions are actually free.
+(head -c 16190 orig/pascal-1.rom; head -c 194 /dev/zero) > build/pascal-1-clean.rom
+(head -c 16212 orig/pascal-2.rom; head -c 172 /dev/zero) > build/pascal-2-clean.rom
